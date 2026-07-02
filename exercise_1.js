0d9b0110ea6a73a6ff5830eacd1b47c91aef3e4c
Day-void <maringisanwaday@gmail.com>
2026-07-02 06:13:27 -0700

    first commit

---
+++ exercise_1.js
@@ -0,0 +1,21 @@
+// const car = {
+// make: "Toyota",
+// model: "Corolla",
+// color: "blue",
+// describe: function() {
+// return (`This is a  ${this.color} ${this.make} ${this.model}.`)
+// }
+// };
+// console.log(car.describe());
+// // TODO: write updateColor(newColor) below
+const car = {
+  make: "Toyota",
+  model: "Corolla",
+  color: "blue",
+  void() {
+    return `This is a ${this.color} ${this.make} ${this.model}.`;
+  },
+
+};
+console.log(car.void());
+// TODO: write updateColor(newColor) below
