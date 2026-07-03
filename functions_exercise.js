f5177e4f4fae77184e675b5f9bdcc61a46724d23
Day-void <maringisanwaday@gmail.com>
2026-07-03 00:53:00 -0700

    function Expressions

--- functions_exercise.js
+++ functions_exercise.js
@@ -1,20 +1,34 @@
-// Functions Declaration
+// // Functions Declaration
 
-function greetGuest(name) {
-  return `Welcome to movie night, ${name}!`;
-}
+// function greetGuest(name) {
+//   return `Welcome to movie night, ${name}!`;
+// }
 
-function calculateTotalSnacks(numGuests, snacksPerGuest) {
-  return numGuests * 2;
-}
+// function calculateTotalSnacks(numGuests, snacksPerGuest) {
+//   return numGuests * 2;
+// }
  
-function isEnoughPizza(numSlices, numGuests) {
-  return numSlices >= numGuests * 3;
-}
+// function isEnoughPizza(numSlices, numGuests) {
+//   return numSlices >= numGuests * 3;
+// }
   
-console.log(greetGuest("Sam"));							
-//	"Welcome	to	movie	night,	Sam!"
-console.log(calculateTotalSnacks(4));	//	8
-console.log(isEnoughPizza(12,	4));				
-//	true
-console.log(isEnoughPizza(10,	4));
+// console.log(greetGuest("Sam"));							
+// //	"Welcome	to	movie	night,	Sam!"
+// console.log(calculateTotalSnacks(4));	//	8
+// console.log(isEnoughPizza(12,	4));				
+// //	true
+// console.log(isEnoughPizza(10,	4));
+
+// Function Expressions
+
+const greetGuest = function(name) {
+  return `Welcome to movie night, ${name}!`;
+};
+
+const calculateTotalSnacks = function(numGuests, snacksPerGuest) {
+  return numGuests * 2;
+};
+
+const isEnoughPizza = function(numSlices, numGuests) {
+  return numSlices >= numGuests * 3;
+};
